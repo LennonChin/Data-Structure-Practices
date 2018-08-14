51fef2ba001dd790f96695f7525b8200bf4285fe
LennonChin <i@coderap.com>
2018-08-14 22:44:46 +0800

    1, implement HashTable resize operation;

--- 14.HashTable/src/main/java/com/coderap/HashTable.java
+++ 14.HashTable/src/main/java/com/coderap/HashTable.java
@@ -10,6 +10,12 @@ import java.util.TreeMap;
  */
 public class HashTable<K, V> {
     
+    // 扩容上界和缩容下界
+    private final static int upperTolerance = 31;
+    private final static int lowerTolerance = 3;
+    // 初始数组容量
+    private final static int defaultCapacity = 7;
+    
     private TreeMap<K, V>[] hashtable;
     private int M;
     private int size;
@@ -24,7 +30,7 @@ public class HashTable<K, V> {
     }
     
     public HashTable() {
-        this(97);
+        this(defaultCapacity);
     }
     
     /**
@@ -57,6 +63,10 @@ public class HashTable<K, V> {
             // 添加操作
             map.put(key, value);
             size++;
+            if (size >= upperTolerance * M) {
+                // 需要扩容，扩容一倍
+                resize(2 * M);
+            }
         }
     }
     
@@ -64,10 +74,39 @@ public class HashTable<K, V> {
         TreeMap<K, V> map = hashtable[hash(key)];
         if (map.containsKey(key)) {
             size--;
+            if (size < lowerTolerance * M && M / 2 > defaultCapacity) {
+                // 需要缩容
+                resize(M / 2);
+            }
             return map.remove(key);
         } else {
             return null;
         }
+    }
+    
+    /**
+     * 重整数组大小
+     *
+     * @param newCapacity
+     */
+    private void resize(int newCapacity) {
+        TreeMap<K, V>[] newHashTable = new TreeMap[newCapacity];
+        for (int i = 0; i < newCapacity; i++) {
+            newHashTable[i] = new TreeMap<>();
+        }
+        
+        // 记录原来的M
+        int loopCount = M;
+        // 将M赋值为新值，因为hash方法中需要用到新的M
+        M = newCapacity;
+        // 循环重组
+        for (int i = 0; i < loopCount; i++) {
+            TreeMap<K, V> map = hashtable[i];
+            for (K key : map.keySet()) {
+                newHashTable[hash(key)].put(key, map.get(key));
+            }
+        }
+        this.hashtable = newHashTable;
     }
     
     public void set(K key, V value) {
